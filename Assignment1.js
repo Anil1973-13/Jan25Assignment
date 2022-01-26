093db85cb71a591081450eb12ec11b39fb1202ac
Anil1973-13 <96384565+Anil1973-13@users.noreply.github.com>
2022-01-26 20:24:54 +0530

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,15 @@
+var a = prompt("Enter the Value of a");
+if(a>10){
+
+console.log("A is greater than 10");
+
+}
+else if(a<10){
+
+console.log("A is less than 10");
+
+}
+
+else{
+console.log("A is equal to 10");
+}
