90bfca2ee466025835713243105221b2379f129c
Anil1973-13 <96384565+Anil1973-13@users.noreply.github.com>
2022-01-26 15:27:11 +0530

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,11 @@
+var a = prompt("Enter the Value of a");
+if(a>10){
+
+console.log("A is greater than 10");
+
+}
+else{
+
+console.log("A is less than 10");
+
+}
---
+++ Assignment2.js
@@ -0,0 +1,25 @@
+var a = prompt("Enter the value of A");
+var b = prompt("Enter the value of B");
+var c = prompt("Enter the value of C");
+
+
+if(a>b && a>c){
+
+console.log("A is greater of all three");
+}
+
+else{
+
+if(b>c){
+
+console.log("B is greater of all three");
+
+
+}
+
+else{
+
+console.log("C is greater of all three");
+}
+
+}
